46a347e8551960fbc5ad6e06f273b1bc5bcabebc
andry300000 <andry.che.2005@gmail.com>
2025-12-25 16:34:07 +0000

    lab3

---
+++ tasks/cheremkhin_a_radix_sort_batcher/mpi/include/ops_mpi.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "cheremkhin_a_radix_sort_batcher/common/include/common.hpp"
+
+namespace cheremkhin_a_radix_sort_batcher {
+
+class CheremkhinARadixSortBatcherMPI : public BaseTask {
+ public:
+  static constexpr ppc::task::TypeOfTask GetStaticTypeOfTask() {
+    return ppc::task::TypeOfTask::kMPI;
+  }
+  explicit CheremkhinARadixSortBatcherMPI(const InType &in);
+
+ private:
+  bool ValidationImpl() override;
+  bool PreProcessingImpl() override;
+  bool RunImpl() override;
+  bool PostProcessingImpl() override;
+};
+
+}  // namespace cheremkhin_a_radix_sort_batcher
---
+++ tasks/cheremkhin_a_radix_sort_batcher/seq/include/ops_seq.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "cheremkhin_a_radix_sort_batcher/common/include/common.hpp"
+
+namespace cheremkhin_a_radix_sort_batcher {
+
+class CheremkhinARadixSortBatcherSEQ : public BaseTask {
+ public:
+  static constexpr ppc::task::TypeOfTask GetStaticTypeOfTask() {
+    return ppc::task::TypeOfTask::kSEQ;
+  }
+  explicit CheremkhinARadixSortBatcherSEQ(const InType &in);
+
+ private:
+  bool ValidationImpl() override;
+  bool PreProcessingImpl() override;
+  bool RunImpl() override;
+  bool PostProcessingImpl() override;
+};
+
+}  // namespace cheremkhin_a_radix_sort_batcher
